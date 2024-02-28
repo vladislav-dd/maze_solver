9e32e017018655de55059e69edff356c688c58cb
vladislav-dd <vladislav.dudko0@gmail.com>
2024-02-28 11:18:46 +0300

    add cell

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-from graphics import Window
+from graphics import Window, Point, Line
+from cell import Cell
 
 def main():
     win = Window(800, 600)
+
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(50, 50, 100, 100)
+
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(125, 125, 200, 200)
+
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(300, 300, 500, 500)
+    
     win.wait_for_close()
 
 
