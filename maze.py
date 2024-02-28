03a7591cbaba7c9bb7186148a4e38663daedcfe3
vladislav-dd <vladislav.dudko0@gmail.com>
2024-02-28 15:07:51 +0300

    Add tests

--- maze.py
+++ maze.py
@@ -3,7 +3,7 @@ import time
 
 
 class Maze():
-    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None):
         self._cells = []
         self._x1 = x1
         self._y1 = y1
