f9fa365212f4612b2b7048e07b661bc96e409b85
vladislav-dd <vladislav.dudko0@gmail.com>
2024-02-28 16:50:56 +0300

    Breaking wall in entrance and exit

--- cell.py
+++ cell.py
@@ -18,17 +18,29 @@ class Cell():
         self._x2 = x2
         self._y2 = y2
         if self.has_left_wall:
-            line = Line(Point(x1, y1), Point(x1,y2))
+            line = Line(Point(x1, y1), Point(x1, y2))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line, "white")
         if self.has_top_wall:
-            line = Line(Point(x1,y1), Point(x2,y1))
+            line = Line(Point(x1, y1), Point(x2, y1))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line, "white")
         if self.has_right_wall:
-            line = Line(Point(x2,y1), Point(x2,y2))
+            line = Line(Point(x2, y1), Point(x2, y2))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line, "white")
         if self.has_bottom_wall:
-            line = Line(Point(x1,y2), Point(x2,y2))
+            line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line, "white")
         
     def draw_move(self, to_cell, undo=False):
         if self._win is None:
--- maze.py
+++ maze.py
@@ -13,6 +13,7 @@ class Maze():
         self._cell_size_y = cell_size_y
         self._win = win
         self._create_cells()
+        self._break_entrance_and_exit()
 
 
     def _create_cells(self):
@@ -40,3 +41,10 @@ class Maze():
             return
         self._win.redraw()
         time.sleep(0.05)
+    
+    def _break_entrance_and_exit(self):
+        self._cells[0][0].has_top_wall = False
+        self._draw_cell(0, 0)
+        self._cells[self._num_cols - 1][self._num_rows - 1].has_bottom_wall = False
+        self._draw_cell(self._num_cols - 1, self._num_rows - 1)
+        self._win.redraw()
